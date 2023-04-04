9fe0091c15848ef71df31bb1742c7166868a71e9
Yongchany <jeffrey0301@naver.com>
2023-04-04 15:54:42 +0900

    0404

--- src/main/java/kr/ac/poly/controller/SampleController.java
+++ src/main/java/kr/ac/poly/controller/SampleController.java
@@ -1,9 +1,10 @@
 package kr.ac.poly.controller;
 
-import ch.qos.logback.core.model.Model;
+
 import kr.ac.poly.dto.SampleDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
