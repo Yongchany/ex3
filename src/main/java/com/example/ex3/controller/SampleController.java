12118dd45d69f579b38b190380b2feeb7f6b07e6
Yongchany <jeffrey0301@naver.com>
2023-04-11 16:22:55 +0900

    0411

--- src/main/java/com/example/ex3/controller/SampleController.java
+++ src/main/java/com/example/ex3/controller/SampleController.java
@@ -1,12 +1,13 @@
-package kr.ac.poly.controller;
+package com.example.ex3.controller;
 
 
-import kr.ac.poly.dto.SampleDTO;
+import com.example.ex3.dto.SampleDTO;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -36,5 +37,38 @@ public class SampleController {
                 }
         ).collect(Collectors.toList());
         model.addAttribute("list", list);
+    }
+
+    @GetMapping("exInline")
+    public String exInline(RedirectAttributes redirectAttributes){
+        SampleDTO dto = SampleDTO.builder()
+                .sno(100L)
+                .first("Firs...100")
+                .last("Last...100")
+                .regTime(LocalDateTime.now())
+                .build();
+        redirectAttributes.addFlashAttribute("result", "Success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+        return "redirect:/sample/ex3";
+    }
+
+    @GetMapping("/ex3")
+    public void ex3(){
+        log.info("ex3() 호출됨");
+    }
+
+    @GetMapping("/exLayout")
+    public void exLayout1(){
+        log.info("exLayout() 호출됨");
+    }
+
+    @GetMapping("/exLayout2")
+    public void exLayout2(){
+        log.info("exLayout2() 호출됨");
+    }
+
+    @GetMapping("/exLayout3")
+    public void exLayout3(){
+        log.info("exLayout3() 호출됨");
     }
 }
